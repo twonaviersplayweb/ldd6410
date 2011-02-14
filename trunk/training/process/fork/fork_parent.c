9729c9d60f592e065ebb35ead5ca2aeb590e1214
21cnbao <21cnbao@2289ec02-a210-11de-bfb1-cf57f9aef694>
2011-02-14 01:04:11 +0000

    split fork.c into fork_parent/child.c

--- trunk/training/process/fork/fork_parent.c
+++ trunk/training/process/fork/fork_parent.c
@@ -5,6 +5,7 @@ int data = 10;
 
 int child_process()
 {
+	sleep(1);
 	printf("Child process %d, data %d\n",getpid(),data);
 	data = 20;
 	printf("Child process %d, data %d\n",getpid(),data);
@@ -15,12 +16,13 @@ int main(int argc, char* argv[])
 {
 	int pid;
 	pid = fork();
-	
+
 	if(pid==0) {
-		child_process();	
+		child_process();
 	}
 	else{
-		sleep(1);
+		//sleep(1);
+		data = 20;
 		printf("Parent process %d, data %d\n",getpid(), data);
 		while(1);
 	}
