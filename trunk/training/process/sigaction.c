dddede9447ee9c5e5f6d2c706dbb7792a1f0c2dd
21cnbao <21cnbao@2289ec02-a210-11de-bfb1-cf57f9aef694>
2011-02-16 06:55:55 +0000

    new sigaction example

--- trunk/training/process/sigaction.c
+++ trunk/training/process/sigaction.c
@@ -1,35 +1,31 @@
-#include <stdio.h>
 #include <signal.h>
-
-void signal_set(struct sigaction *act)
+#include <sys/types.h>
+#include <unistd.h>
+void new_op(int,siginfo_t*,void*);
+int main(int argc,char**argv)
 {
-        switch(act->sa_flags){
-		case (int)SIG_DFL:
-			printf("using default hander\n");
-			break;
-		case (int)SIG_IGN:
-			printf("ignore the signal\n"); 
-			break;
-		default: 
-			printf("%0x\n",act->sa_handler);
-     	}
+	struct sigaction act;	
+	int sig;
+	sig=atoi(argv[1]);
+	
+	sigemptyset(&act.sa_mask);
+	act.sa_flags=SA_SIGINFO;
+	act.sa_sigaction=new_op;
+	
+	if(sigaction(sig,&act,NULL) < 0)
+	{
+		printf("install sigal error\n");
+	}
+	
+	while(1)
+	{
+		sleep(2);
+		printf("wait for the signal\n");
+	}
+}
+void new_op(int signum,siginfo_t *info,void *myact)
+{
+	printf("receive signal %d", signum);
+//	sleep(5);
 }
 
-int main(int argc,char** argv)
-{
-        int i;
-        struct sigaction act,oldact;
-        
-	act.sa_handler = signal_set;
-        act.sa_flags = SA_NODEFER| SA_RESETHAND;
-        
-	sigaction (SIGUSR1,&act,&oldact) ;
-
-	for (i=1; i<12; i++){
-                printf("signal %d handler is : ",i);
-                sigaction (i,NULL,&oldact) ;
-                signal_set (&oldact);
-        }
-
-        return 0;
-}
