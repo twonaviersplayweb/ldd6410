57bb36fdbacca272518931c2ab386ea0835a5ec7
21cnbao <21cnbao@2289ec02-a210-11de-bfb1-cf57f9aef694>
2011-04-30 16:02:19 +0000

    add cunit lint typeof zero-array example

---
+++ training/program-c/lint/lint-exam.c
@@ -0,0 +1,13 @@
+int main(int argc,char* argv[])
+{
+
+	int a=100; /*没有使用的变量*/
+	int b[8];
+	printf("Hello c\n");
+	b[9]=100; /*明显数组越界 */
+
+	/* 用到了两个为声明的变量c和d */
+	   c=100;
+	   d=10;
+	   return 0;
+}
