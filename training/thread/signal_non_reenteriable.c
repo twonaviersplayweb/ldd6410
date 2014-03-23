f6042e1ffb96ae5bfa88550a8f75f15a11dd6c27
21cnbao <21cnbao@gmail.com>
2014-03-23 03:13:03 +0000

    add a pthread cond/wait produce/consume example

--- training/thread/signal_non_reenteriable.c
+++ training/thread/signal_non_reenteriable.c
@@ -6,14 +6,9 @@ void signal_handler(int signum){
 	alarm (1);
 }
 
-void signal_handler1(int signum){
-	printf ("received CTRL+C\n");
-}
-
 int main (void){
 	static struct two_int zeros = { 0, 0 }, ones = { 1, 1 };
 	signal (SIGALRM, signal_handler);
-	signal (9, signal_handler1);
 	data = zeros;
 	alarm (1);
 	while (1)
