fceee19780081fd6104a96359575b25867a8eb31
21cnbao@gmail.com <21cnbao@gmail.com@2289ec02-a210-11de-bfb1-cf57f9aef694>
2014-03-28 13:52:11 +0000

    splint: example with more static check issues

--- trunk/training/program-c/lint/lint-exam.c
+++ trunk/training/program-c/lint/lint-exam.c
@@ -1,13 +1,55 @@
-int main(int argc,char* argv[])
+#include<stdlib.h>
+#include<stdio.h>
+
+/*Program: typeerr.c -- use splint to check type varible error */
+int foo(int i, char *s, bool b1, bool b2)
 {
+	if (i = 3)
+		return b1;
+	if (!i || s)
+		return i;
+	if (s)
+		return 7;
+	if (b1 == b2)
+		return 3;
+	return 2;
+}
 
-	int a=100; /*没有使用的变量*/
-	int b[8];
-	printf("Hello c\n");
-	b[9]=100; /*明显数组越界 */
-
-	/* 用到了两个为声明的变量c和d */
-	c=100;
-	d=10;
+/*Program: overflow -- splint check overflow error */
+int overflow()
+{
+	int buf[10];
+	buf[10] = 3;
 	return 0;
+}
+
+/*Program: bound.c -- use splint checking bound overflow error */
+void updateEnv(char *str)
+{
+	char *tmp;
+	tmp = getenv("MYENV");
+	if (tmp != NULL)
+		strcpy(str, tmp);
+}
+
+void updateEnvSafe(char *str, size_t strSize)
+{
+	char *tmp;
+	tmp = getenv("MYENV");
+	if (tmp != NULL) {
+		strncpy(str, tmp, strSize - 1);
+		str[strSize - 1] = '\0';
+	}
+}
+
+/*Program: malloc1.c -- check varible type */
+int main()
+{
+	char *some_mem;
+	int size1 = 1048567;
+	//size_t size1 = 1048567;
+	some_mem = (char *)malloc(size1);
+	printf("Malloced 1M Memory!\n");
+	free(some_mem);
+	exit(EXIT_SUCCESS);
 }
